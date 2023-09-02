fda2c8d1c121d61a83287b11fcc6670fedbcbc26
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-09-02 13:47:44 +0530

    Time: 691 ms (10.14%) | Memory: 274.6 MB (5.04%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -13,13 +13,6 @@ public:
         for(int i= ind; i < n; i++){
             temp += s[i];
             int chk = rec(i + 1, s, um, n, dp);
-            // dp[i] = chk;
-            // if(dp.find(s.substr(ind + 1, n - temp.size())) == dp.end()){
-            //     chk = 
-            //     dp[s.substr(ind + 1, n - temp.size())] = chk;
-            // }
-            // else
-            //     chk = dp[s.substr(ind + 1, n - temp.size())];
 
             int cur = temp.size();
             if(um[temp] > 0)
@@ -40,9 +33,7 @@ public:
             um[x]++;
         }
         int ans = rec(0, s, um, n, dp);
-        // for(auto & x : dp){
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
+        
         return ans;
     }
 };
