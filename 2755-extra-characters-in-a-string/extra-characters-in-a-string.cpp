800b336cded63b794f9d7948cdb4cc9ddfd37778
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-09-02 13:57:10 +0530

    Time: 354 ms (23.77%) | Memory: 206.5 MB (13.81%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -27,13 +27,32 @@ public:
     int minExtraChar(string s, vector<string>& dic) {
         int n = s.size();
         unordered_map<string, int> um;
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1, 0);
 
         for(string x : dic){
             um[x]++;
         }
-        int ans = rec(0, s, um, n, dp);
-        
-        return ans;
+
+        // int ans = rec(0, s, um, n, dp);
+
+        for(int i = n-1; i >= 0; i--){
+            string temp = "";
+            int ans = s.size();
+            for(int j= i; j < n; j++){
+                temp += s[j];
+                // int chk = rec(j + 1, s, um, n, dp);
+                int chk = dp[j + 1];
+
+                int cur = temp.size();
+                if(um[temp] > 0)
+                    cur = 0;
+
+                ans = min(ans, cur + chk);
+            }
+
+            dp[i] = ans;
+        }
+
+        return dp[0];
     }
 };
