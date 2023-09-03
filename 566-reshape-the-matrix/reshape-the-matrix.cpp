2e4666f9f9396e6d2bf5df99f08fc4be605ab8a5
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-09-03 12:11:49 +0530

    Time: 8 ms (73.06%) | Memory: 10.6 MB (92.24%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -7,15 +7,24 @@ public:
             return mat;
 
         vector<vector<int>> res(r, vector<int>(c, 0));
-        vector<int> v;
+        // vector<int> v;
+        int q = 0, w = 0;
+        for(int i  =0 ;i < m;i++){
+            for(int j = 0; j < n; j++){
+                if(w >= c){
+                    w = 0;
+                    q++;
+                }
+                res[q][w++] = mat[i][j];
+            }
+        }
+                // v.push_back(mat[i][j]);
 
-        for(int i  =0 ;i < m;i++)
-            for(int j = 0; j < n; j++)
-                v.push_back(mat[i][j]);
-        int k = 0;
-        for(int i =0 ; i < r; i++)
-            for(int j = 0; j < c; j++)
-                res[i][j] = v[k++];
+
+        // int k = 0;
+        // for(int i =0 ; i < r; i++)
+        //     for(int j = 0; j < c; j++)
+        //         res[i][j] = v[k++];
             
         
         return res;
