3d0c5a8e8bc78db198a19b470ac01847ec9ec27f
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-09-03 16:55:43 +0530

    Time: 3 ms (37.76%) | Memory: 7.3 MB (17.51%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -3,13 +3,16 @@ public:
     int maxChunksToSorted(vector<int>& arr) {
         int ans = 0, n = arr.size();
         
+        // we are precalculating the array with max prefix.
         vector<int> v(n, -1);
-        int cm = arr[0];
+        int curmax = arr[0];
         for(int i = 0; i < n; i++){
-            v[i] = max(cm, arr[i]);
-            cm = max(cm, arr[i]);
+            v[i] = max(curmax, arr[i]);
+            curmax = max(curmax, arr[i]);
         }
 
+
+        // now we are checking if the index is equal to the value present in that index, if not then they belong to the same chunk.
         int j = 0;
 
         while(j < n){
