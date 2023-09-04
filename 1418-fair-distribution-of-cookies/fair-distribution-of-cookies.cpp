57f18f604d132a022debae5704a23dc92940b1f5
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-09-04 10:39:18 +0530

    Time: 1114 ms (54.48%) | Memory: 7 MB (10.84%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
 
-    int rec(int ind, int &val, vector<int> &ch, vector<int>& cookies, int n, int k){
+    void rec(int ind, int &val, vector<int> &ch, vector<int>& cookies, int n, int k){
         if(ind == n){
             int mx, mn;
             mx = *max_element(ch.begin(), ch.end());
             val = min(mx, val);
-            // mn = *min_element(ch.begin(), ch.end());
-            // vp.push_back(mx);
-            // return mx - mn;
-            return mx;
+            return;
         }
 
-        // if(dp[ind] != -1)
-        //     return dp[ind];
-
-        int ans = INT_MAX;
         for(int i = 0;i < k; i++){
             ch[i] += cookies[ind];
-            int chk = rec(ind + 1, val, ch, cookies, n, k);
-            ans = min(ans, chk);
+            rec(ind + 1, val, ch, cookies, n, k);
             ch[i] -= cookies[ind];
         }
-
-        return ans;
     }
 
     int distributeCookies(vector<int>& cookies, int k) {
@@ -31,14 +21,9 @@ public:
 
         vector<int> ch(k, 0);
 
-        // vector<int> dp(10, -1);
-        // vector<pair<int, int>> vp;
         int val = INT_MAX;
-        int ans = rec(0, val, ch, cookies, n, k);
+        rec(0, val, ch, cookies, n, k);
         
-        // sort(vp.begin(), vp.end());
-        // for(int i = 0;i < vp.size(); i++)
-        //     cout<<vp[i].first<<" "<<vp[i].second<<endl;
         return val;
     }
 };
