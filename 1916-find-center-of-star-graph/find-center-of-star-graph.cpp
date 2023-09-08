1a5ad28b89b0da6697734bd0b47153c3288684f0
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-09-08 23:06:41 +0530

    Time: 160 ms (66.71%) | Memory: 67.2 MB (73.50%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size();
-        unordered_map<int, int> um;
-
-        int ans = -1, mx = INT_MIN;
-        for(int i =0; i < n; i++){
-            um[edges[i][0]]++;
-            um[edges[i][1]]++;
-        }
-
-        for(auto x : um){
-            if(x.second > mx){
-                mx = x.second;
-                ans = x.first;
-            }
-        }
-
-        return ans;
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
+        else
+            return edges[0][1];
     }
 };
