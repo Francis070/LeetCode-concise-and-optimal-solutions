86099c2c689a97447fb5157d1e13e7f47eab67cb
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-09-11 10:42:17 +0530

    Time: 4 ms (21.24%) | Memory: 6 MB (50.20%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        long long cnt =0;
-        long long k = abs(n);
-        // n = abs(n);
-        // cout<<k;
-        while(k != 0){
-            if(k & 1 == 1)
-                cnt++;
-            k >>= 1;
-        }
-
-        if(cnt == 1 && n > 0)
-            return true;
-        return false;
+        return (n > 0) && ((n & (n-1)) == 0);
     }
 };
-
-// -2147483648
