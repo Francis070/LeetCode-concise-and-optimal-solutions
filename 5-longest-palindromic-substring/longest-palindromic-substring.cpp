bcab42b7ab76dba80a275aadddc096f7926f5eec
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-10-27 08:13:12 +0530

    Time: 7 ms (94.92%) | Memory: 9.8 MB (62.09%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -5,9 +5,8 @@ public:
         int ans = 0;
         string ans_str;
         for(int i = 0; i< n; i++){
-            string temp = "";
             //for odd length
-            int x = i-1, y = i +1;
+            int x = i-1, y = i +1;  // 2 pointers to check the values present for each index from the ith index taken as center.
             int cur = 1;
             while(x >= 0 && y < n && s[x] == s[y]){
                 x--; y++;
@@ -20,9 +19,8 @@ public:
             }
 
             //for even length
-
             cur = 0;
-            x = i-1; y = i;
+            x = i-1; y = i;   // 2 pointers to check the values present for each index taking i-1 and i as center.
             while(x >= 0 && y < n && s[x] == s[y]){
                 x--; y++;
                 cur+=2;
