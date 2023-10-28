b83ed644474f442f7f6cd25b02ed65215742664f
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-10-28 12:37:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (86.54%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
 
     double get_ans(string num, string den, bool &chk, vector<vector<string>>& equations, vector<double>& values, vector<int> & vis){
-        // vis[pos] = 1;   
         int n = values.size();
-        // int ans = 1.0;
 
         for(int i = 0; i < n; i++){
             if(vis[i] == 0 && (equations[i][0] == num || equations[i][1] == num)){
                 
                 vis[i] = 1;
-                // cout<<i<<endl;
-                // for(int t = 0; t < n; t++)
-                //     cout<<vis[t]<<" ";
-                // cout<<endl;
                 if(equations[i][0] == den || equations[i][1] == den){
                     chk = true;
                     if(num == den)
@@ -31,9 +25,6 @@ public:
                             double ser = get_ans(equations[i][1], den, chk, equations, values, vis);
                             if(chk)
                                 return values[i] * ser;
-                            // return values[i] * get_ans(equations[i][1], den, chk, equations, values, vis);
-                            // if(chk)
-                            // return ans;
                         }
                         else{
                             double ser =  get_ans(equations[i][0], den, chk, equations, values, vis);
@@ -44,22 +35,10 @@ public:
                         
                     }
                 }
-                // vis[i] = 0;
             }
         }
 
         return 1.0;
-    }
-
-    vector<int> get_sources(vector<vector<string>>& equations, string num){
-        int n = equations.size();
-        vector<int> src;
-        for(int i = 0; i < n; i++){
-            if(equations[i][0] == num || equations[i][1] == num)
-                src.push_back(i);
-        }
-
-        return src;
     }
 
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
@@ -71,15 +50,6 @@ public:
             string num = queries[i][0];
             string den = queries[i][1];
 
-            // vector<int> srcs = get_sources(equations, num);
-
-            // for(int j = 0; j < srcs.size(); j++){
-            //     
-
-            //     double cur = get_ans(num, den, srcs[j], equations, values, vis);
-            //     if(cur != -1.0)
-            //         ans[i] = cur;
-            // }
             vector<int> vis(n, 0);
             bool chk = false;
             double cur = get_ans(num, den, chk, equations, values, vis);
