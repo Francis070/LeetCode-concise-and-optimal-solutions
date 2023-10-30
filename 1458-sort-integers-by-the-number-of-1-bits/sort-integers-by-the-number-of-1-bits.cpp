dd753fda486d1cfb5d537500acd5c8682815c3ad
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-10-30 09:03:08 +0530

    Time: 6 ms (76.61%) | Memory: 10.5 MB (66.29%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,45 +1,9 @@
 class Solution {
 public:
-
-    static int calc_bits(int a){
-        int res = 0;
-        while(a > 0){
-            if(a & 1)
-                res++;
-            a >>= 1;
-        }
-        return res;
-    }
-
-    // static bool comp(pair<int, int> a, pair<int, int> b){
-    //     // int x = calc_bits(a)
-    //     if(a.second != b.second)
-    //         return a.second < b.second;
-    //     return a.first < b.first;
-    // }
-
-    static bool comp(int a, int b){
-        int x = calc_bits(a), y = calc_bits(b);
-        if(x != y)
-            return x < y;
-        return a < b;
-    }
-
     vector<int> sortByBits(vector<int>& arr) {
-        // int n = arr.size();
-
-        // vector<pair<int, int>> vp(n);
-        // vector<int> ans(n);
-        // for(int i = 0; i < n; i++){
-        //     vp[i] = {arr[i], calc_bits(arr[i])};
-        // }
-
-        // sort(vp.begin(), vp.end(), comp);
-        // for(int i = 0; i < n; i++){
-        //     ans[i] = vp[i].first;
-        // }
-        sort(arr.begin(), arr.end(), comp);
-
-        return arr;
+    sort(arr.begin(), arr.end(),[](const int& a, const int& b){
+        int countA = __builtin_popcount(a), countB = __builtin_popcount(b);
+        return countA==countB ? a<b:countA<countB;});
+    return arr;
     }
 };
