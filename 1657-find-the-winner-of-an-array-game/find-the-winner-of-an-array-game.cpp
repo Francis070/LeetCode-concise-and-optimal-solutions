f9b149f66983c253416b22a33566d2422e06f591
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-11-05 10:41:07 +0530

    Time: 127 ms (7.41%) | Memory: 69.2 MB (12.96%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -5,29 +5,37 @@ public:
 
         int mx = *max_element(arr.begin(), arr.end());
 
-        int cur = arr[0];
-        queue<int> q;
-        for(int i = 1;i < n; i++)
-            q.push(arr[i]);
+        vector<int> nge(n, -1), ge(n, -1);
+        stack<int> st;
 
-        // unordered_map<int, int> um;
-        int cnt = 0;
-        while(cur != mx){
-            int x = q.front();
-            if(x < cur){
-                q.push(x);
-                cnt++;
+        for(int i = 0 ; i < n; i++){
+            // if(arr[i] == mx)
+            //     break;
+            // else{
+                while(!st.empty() && arr[st.top()] < arr[i]){
+                    nge[st.top()] = i;
+                    st.pop();
+                }
+
+                st.push(i);
+            // }
+        }
+        int cur = arr[0];
+        ge[0] = INT_MAX;
+        for(int i =1; i< n; i++){
+            ge[i] = cur;
+            cur = max(cur, arr[i]);
+        }
+
+        for(int i =0; i< n; i++){
+            if(arr[i] != mx){
+                // cout<<ge[i]<<" "<<nge[i]<<endl;
+                if((ge[i] < arr[i] && (nge[i] - i - 1) >= k-1) || (ge[i] == INT_MAX && (nge[i] - i - 1) >= k)){
+                    return arr[i];
+                }
             }
-            else{
-                q.push(cur);
-                cur = x;
-                cnt = 1;
-            }
-            q.pop();
-            // um[cur]++;
-            if(cnt == k){
-                return cur;
-            } 
+            else
+                break;
         }
 
         return mx;
