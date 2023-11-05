3fdd23704bff194414957ef645d69ee15669bafa
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-11-05 10:47:13 +0530

    Time: 90 ms (48.15%) | Memory: 63.3 MB (93.06%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        int n = arr.size();
-
-        int mx = *max_element(arr.begin(), arr.end());
-
-        vector<int> nge(n, -1), ge(n, -1);
-        stack<int> st;
-
-        for(int i = 0 ; i < n; i++){
-            // if(arr[i] == mx)
-            //     break;
-            // else{
-                while(!st.empty() && arr[st.top()] < arr[i]){
-                    nge[st.top()] = i;
-                    st.pop();
-                }
-
-                st.push(i);
-            // }
-        }
-        int cur = arr[0];
-        ge[0] = INT_MAX;
-        for(int i =1; i< n; i++){
-            ge[i] = cur;
-            cur = max(cur, arr[i]);
-        }
-
-        for(int i =0; i< n; i++){
-            if(arr[i] != mx){
-                // cout<<ge[i]<<" "<<nge[i]<<endl;
-                if((ge[i] < arr[i] && (nge[i] - i - 1) >= k-1) || (ge[i] == INT_MAX && (nge[i] - i - 1) >= k)){
-                    return arr[i];
-                }
+        int cur = arr[0], n = arr.size();
+        int win= 0;
+        for(int i =1; i < n; i++){
+            if(cur < arr[i]){
+                cur = arr[i];
+                win = 0;
             }
-            else
-                break;
+            if(++win >= k){
+                return cur;
+            }
         }
 
-        return mx;
+        return *max_element(arr.begin(), arr.end());
     }
 };
