b9b2b64207fad396ec0a5280d6df57ed0a3d6c3f
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-11-11 19:30:05 +0530

    Time: 2 ms (46.25%) | Memory: 6.4 MB (40.62%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        double kel = celsius + 273.15;
-        double far = (celsius * 1.80) + 32.0;
-
-        return {kel, far};
+        return {celsius + 273.15, (celsius * 1.80) + 32.0};
     }
 };
