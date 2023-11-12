66ace250caf0b3bdb85fc361384085c114359546
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-11-12 21:42:16 +0530

    Time: 2484 ms (5.01%) | Memory: 55.6 MB (87.73%) - LeetSync

---
+++ 833-bus-routes/bus-routes.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        int n = routes.size();
+        if(source == target)
+            return 0;
+
+        unordered_map<int, vector<int>> um;
+        set<int> vis;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < routes[i].size(); j++){
+                um[routes[i][j]].push_back(i);
+            }
+        }
+
+        vector<int> q;
+
+        for(int i = 0; i < um[source].size(); i++){
+            q.push_back(um[source][i]);
+            vis.insert(um[source][i]);
+        }
+        int cnt = 1;
+        bool dest = false;
+        while(q.size() > 0){
+            int sz = q.size();
+            // vector<int> temp;
+            for(int i = 0; i < sz; i++){
+                int rn = q[i];
+                for(int j = 0; j < routes[rn].size(); j++){
+                    if(routes[rn][j] == target){
+                        return cnt;
+                    }
+                    else{
+                        int node = routes[rn][j];
+                        for(int k = 0; k < um[node].size(); k++){
+                            if(vis.find(um[node][k]) == vis.end()){
+                                q.push_back(um[node][k]);
+                                vis.insert(um[node][k]);
+                            }
+                        }
+                    }
+                }
+            }
+            // q.clear();
+            // q = temp;
+            // temp.clear();
+            while(sz > 0){
+                q.erase(q.begin());
+                sz--;
+            }
+            cnt++;
+        }
+
+        return -1;
+    }
+};
