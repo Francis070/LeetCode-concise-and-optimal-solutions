83647c391f3173060ce7ff8a404f51c6922da585
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-11-16 09:35:24 +0530

    Time: 3 ms (74.62%) | Memory: 10.8 MB (52.42%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,56 +1,26 @@
 class Solution {
 public:
-
-    string change(int er, int req){
-        // cout<<er<<endl;
-        string res = "";
-        while(er > 0){
-            if(er & 1){
-                res = '1' + res;
-            }
-            else{
-                res = '0' + res;
-            }
-            er >>= 1;
-        }
-        // int 
-        // cout<<res<<endl;
-        if(res.size() < req){
-            int rem = req - res.size();
-            for(int i = 0; i < rem; i++){
-                res = "0" + res;
-            }
-        }
-        // for(int i = 0; i <= (req - res.size()); i++){
-        //     res = '0' + res;
-        // }
-        return res;
-    }
-
-    bool find(string st, vector<string>& nums){
-        int n = nums.size();
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] == st)
-                return false;
+    string ans = "";
+    void rec(int ind, int n, string cur, unordered_set<string> &st){
+        if(ind == n){
+            if(st.find(cur) == st.end())
+                ans = cur;
+            return;
         }
 
-        return true;
+        rec(ind + 1, n, cur + '0', st);
+        if(ans != "")
+            return ;
+        rec(ind + 1, n, cur + '1', st);
+        if(ans != "")
+            return;
     }
 
     string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums.size(), m = nums[0].size();
+        unordered_set<string> st(nums.begin(), nums.end());
 
-        int min_v = 0, max_v = pow(2, m) - 1;
-        // cout<<min_v<<" "<<max_v<<endl;
+        rec(0, nums.size(), "", st);
 
-        for(int i = min_v; i <= max_v; i++){
-            string get_st = change(i, m);
-            // cout<<get_st<<endl;
-            if(find(get_st, nums))
-                return get_st;
-        }
-
-        return "";
+        return ans;
     }
 };
