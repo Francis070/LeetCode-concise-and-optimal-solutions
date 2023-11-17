49bb11e39abca640be315cba6aaa1bc687027e96
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-11-17 07:20:24 +0530

    Time: 199 ms (18.08%) | Memory: 96.7 MB (18.77%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -4,9 +4,13 @@ public:
         int n = nums.size();
 
         sort(nums.begin(), nums.end());
-        int ans = INT_MIN;
-        for(int i = 0; i < n/2; i++){
+        int ans = INT_MIN, i = 0;
+        // for(int i = 0; i < n/2; i++){
+        //     ans = max(ans, nums[i] + nums[n-i-1]);
+        // }
+        while(i < n/2){
             ans = max(ans, nums[i] + nums[n-i-1]);
+            i++;
         }
 
         return ans;
