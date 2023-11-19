9ffec75c85e902801670afaee48025bcbdf280a3
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-11-19 08:57:22 +0530

    Time: 159 ms (66.26%) | Memory: 83.1 MB (88.96%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -4,12 +4,9 @@ public:
         int n = nums.size();
 
         sort(nums.begin(), nums.end(), greater<int>());
-        int cnt = 1, ans = 0;
+        int ans = 0;
         for(int i = 1; i< n; i++){
             if(nums[i] != nums[i-1]){
-            //     cnt++;
-            // }
-            // else{
                 ans += (i);
             }
         }
