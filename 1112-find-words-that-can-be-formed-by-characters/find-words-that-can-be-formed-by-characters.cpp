7fa4db862d69db79436961dfdb606c832b6a36b4
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-02 08:08:59 +0530

    Time: 38 ms (91.73%) | Memory: 15.7 MB (91.73%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
         int n = words.size();
-        unordered_map<char, int> um;
+        // unordered_map<char, int> um;
+        int um[26] = {0};
         for(char c : chars){
-            um[c]++;
+            um[c - 'a']++;
         }
         int ans =0;
         for(int i = 0; i < n; i++){
-            unordered_map<char, int> temp;
+            // unordered_map<char, int> temp;
+            int temp[26] = {0};
             int sz = words[i].size();
             bool chk = true;
             for(int k = 0; k < sz; k++){
-                temp[words[i][k]]++;
+                temp[words[i][k] - 'a']++;
 
-                if(temp[words[i][k]] > um[words[i][k]]){
+                if(temp[words[i][k] - 'a'] > um[words[i][k] - 'a']){
                     chk = false;
                     break;
                 }
