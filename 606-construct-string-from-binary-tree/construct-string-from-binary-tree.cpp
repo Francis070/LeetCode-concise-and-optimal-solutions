06f138acb84b93253fb4b6aad6de86f72ff604ed
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-08 07:54:30 +0530

    Time: 15 ms (62.91%) | Memory: 23.9 MB (70.39%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void rec(TreeNode * root, string & ans){
+
+        if(root == NULL){
+            return;
+        }
+
+        ans += to_string(root->val);
+        
+        if(root->left != NULL || (root->left == NULL && root->right != NULL)){
+            ans += '(';
+            rec(root->left, ans);
+            ans += ')';
+        }
+        // else if(root->left == NULL && root->right != NULL){
+        //     ans += '()';
+        // }
+
+        if(root->right != NULL){
+            ans += '(';
+            rec(root->right, ans);
+            ans += ')';
+        }
+
+        // if(ans.back() != '(' || ans.size() > 1)
+        //     ans += ')';
+    }
+
+    string tree2str(TreeNode* root) {
+        string ans = "";
+
+        rec(root, ans);
+
+        return ans;
+    }
+};
