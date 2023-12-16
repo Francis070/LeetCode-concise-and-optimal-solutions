d8a2bfef2d4d1be66706b3cd9b62b474df1e8e1d
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-16 14:18:18 +0530

    Time: 11 ms (45.06%) | Memory: 7.8 MB (45.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int ss = s.size(), ts = t.size();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+        // int ss = s.size(), ts = t.size();
 
-        if(ss != ts) return false;
+        // if(ss != ts) return false;
 
-        unordered_map<char, int> um;
-        for(char x : t){
-            um[x]++;
-        }
+        // unordered_map<char, int> um;
+        // for(char x : t){
+        //     um[x]++;
+        // }
 
-        for(int i = 0; i < ss; i++){
-            if(um.find(s[i]) == um.end())
-                return false;
-            else{
-                um[s[i]]--;
-                if(um[s[i]] == 0)
-                    um.erase(s[i]);
-            }
-        }
+        // for(int i = 0; i < ss; i++){
+        //     if(um.find(s[i]) == um.end())
+        //         return false;
+        //     else{
+        //         um[s[i]]--;
+        //         if(um[s[i]] == 0)
+        //             um.erase(s[i]);
+        //     }
+        // }
 
-        return true;
+        // return true;
     }
 };
