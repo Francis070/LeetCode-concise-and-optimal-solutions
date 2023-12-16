93f83e084bd99ed99655371ab7b599234c82a903
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-16 16:55:59 +0530

    Time: 132 ms (89.87%) | Memory: 83.5 MB (10.93%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -3,49 +3,15 @@ public:
     int partitionArray(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        map<int, int> m;
-
-        for(int i =0; i < n; i++){
-            if(m.size() == 0){
-                m[nums[i]] = nums[i];
-            }
-            else{
-                auto x = m.lower_bound(nums[i]);
-                if(x == m.end()){
-                    x = prev(x);
-                }
-                // cout<<i<<" "<<nums[i]<<" "<<x->first<<" "<<x->second<<endl;
-                if(abs(x->first - nums[i]) <= k && abs(x->second - nums[i]) <= k){
-                    if(nums[i] < x->first){
-                        int y = x->second;
-                        m.erase(x);
-                        m[nums[i]] = y;
-                    }
-                    else{
-                        x->second = max(x->second, nums[i]);
-                    }
-                }
-                else if(x != m.begin() && (abs(prev(x)->first - nums[i]) <= k && abs(prev(x)->second - nums[i]) <= k)){
-                    x = prev(x);
-                    if(nums[i] < x->first){
-                        int y = x->second;
-                        m.erase(x);
-                        m[nums[i]] = y;
-                    }
-                    else{
-                        x->second = max(x->second, nums[i]);
-                    }
-                }
-                else{
-                    m[nums[i]] = nums[i];
-                }
+        int st = nums[0];
+        int ans = 1;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - st > k){
+                ans ++;
+                st = nums[i];
             }
         }
 
-        // for(auto &x : m){
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
-
-        return m.size();
+        return ans;
     }
 };
