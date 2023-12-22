ee8348361756aba2e473886d2fd6148063ed39cf
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-22 19:16:19 +0530

    Time: 31 ms (79.20%) | Memory: 30.9 MB (9.89%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<pair<int, int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    vector<pair<int, int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};  // directions
 
     void bfs(int x, int y, int ocn, vector<vector<pair<int, int>>> &chk, vector<vector<int>>& ht){
         queue<pair<int, int>> q;
@@ -21,7 +21,7 @@ public:
                             q.push({a, b});
                         }
                     }
-                    else if(ocn == 2){
+                    else{
                         if((ht[cur.first][cur.second] <= ht[a][b]) && (chk[a][b].second == 0)){
                             chk[a][b].second = 1;
                             q.push({a, b});
@@ -36,39 +36,31 @@ public:
         int n = ht.size();
         int m = ht[0].size();
 
-        vector<vector<int>> ans;
-        vector<vector<pair<int, int>>> chk(n, vector<pair<int, int>>(m, {0, 0}));
+        vector<vector<int>> ans; // to store the cells from which water flows to both the ocean.
+        vector<vector<pair<int, int>>> chk(n, vector<pair<int, int>>(m, {0, 0})); // used to check if water can flow to both the ocean
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(i == 0 || j == 0 || i == n-1 || j == m-1){
-                    if((i == 0 || j == 0) && (i == n-1 || j == m-1)){
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){   // checking from the edges that touch the oceans.
+                    if((i == 0 || j == 0) && (i == n-1 || j == m-1)){  // if the cell touches both the ocean.
                         chk[i][j].first = 1;
                         chk[i][j].second = 1;
                         bfs(i, j, 1, chk, ht);
                         bfs(i, j, 2, chk, ht);
                     }
-                    else if(i == 0 || j == 0){
+                    else if(i == 0 || j == 0){ // if it touches the pacific.
                         chk[i][j].first = 1;
                         bfs(i, j, 1, chk, ht);
                     }
-                    else if(i == n-1 || j == m-1){
+                    else if(i == n-1 || j == m-1){  // if it touches the atlantic.
                         chk[i][j].second = 1;
                         bfs(i, j, 2, chk, ht);
                     }
-                }
-                else{
-                    continue;
                 }
             }
         }
 
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j< m; j++){
-        //         cout<<"("<<chk[i][j].first<<","<<chk[i][j].second<<") ";
-        //     }
-        //     cout<<endl;
-        // }
-
+        // checking whether both the ocean in the chk matrix is true or not
         for(int i = 0; i < n; i++){
             for(int j = 0; j< m; j++){
                 if(chk[i][j].first && chk[i][j].second){
