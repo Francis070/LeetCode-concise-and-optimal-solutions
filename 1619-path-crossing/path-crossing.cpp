beedfbe98e6501d9daa70c03a42a2aebedc526aa
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-23 10:27:59 +0530

    Time: 4 ms (38.95%) | Memory: 7.1 MB (64.64%) - LeetSync

--- 1619-path-crossing/path-crossing.cpp
+++ 1619-path-crossing/path-crossing.cpp
@@ -7,34 +7,15 @@ public:
         int x = 0, y = 0;
         us.insert({0, 0});
         for(int i = 0; i < n; i++){
-            if(path[i] == 'N'){
-                y++;
-                if(us.find({x, y}) != us.end()){
-                    return true;
-                }
-                us.insert({x, y});
-            }
-            else if(path[i] == 'E'){
-                x++;
-                if(us.find({x, y}) != us.end()){
-                    return true;
-                }
-                us.insert({x, y});
-            }
-            else if(path[i] == 'W'){
-                x--;
-                if(us.find({x, y}) != us.end()){
-                    return true;
-                }
-                us.insert({x, y});
-            }
-            else{
-                y--;
-                if(us.find({x, y}) != us.end()){
-                    return true;
-                }
-                us.insert({x, y});
-            }
+            if(path[i] == 'N') y++;
+            else if(path[i] == 'E') x++;
+            else if(path[i] == 'W') x--;
+            else y--;
+                
+            if(us.find({x, y}) != us.end())
+                return true;
+            
+            us.insert({x, y});
         }
         return false;
     }
