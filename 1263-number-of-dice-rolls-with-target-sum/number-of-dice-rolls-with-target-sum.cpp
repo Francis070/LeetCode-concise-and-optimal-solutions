ef4920a366cd2caccc28ae0c4155a625bf822270
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-26 11:59:28 +0530

    Time: 80 ms (7.60%) | Memory: 8.8 MB (45.95%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -22,8 +22,27 @@ public:
     }
 
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+ 1, vector<int>(target + 1, -1));
-        long long ans = rec(n, k, target, dp);
-        return ans%mod;
+        // vector<vector<int>> dp(n+ 1, vector<int>(target + 1, -1));
+        vector<vector<int>> dp(n+ 1, vector<int>(target + 1, 0));
+        // long long ans = rec(n, k, target, dp);
+        // return ans%mod;
+
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= target; j++){
+                for(int t = 1; t <= k; t++){
+                    if(j - t >= 0){
+                        dp[i][j] = (dp[i][j] + dp[i-1][j - t])%mod;
+                    }
+                }
+                    
+                    // ans = (ans + rec(n-1, k, target - t, dp)) % mod;
+            }
+        }
+        // for(int i = 1; i <= k; i++)
+        //     ans = (ans + rec(n-1, k, target - i, dp)) % mod;
+
+        // return dp[n][target] = ans%mod;
+        return dp[n][target];
     }
 };
