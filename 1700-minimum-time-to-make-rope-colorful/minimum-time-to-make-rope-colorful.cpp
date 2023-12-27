d01d6130c67a01ab778ca585f7cf7592b7df4788
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2023-12-27 10:45:20 +0530

    Time: 96 ms (97.62%) | Memory: 95.8 MB (49.48%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -4,27 +4,22 @@ public:
         int n = colors.size();
 
         int ans = 0;
-        // priority_queue<int, vector<int>, greater<int>> pq;
-        // priority_queue<int> pq;
 
         for(int i = 1; i < n; i++){
             if(colors[i] == colors[i-1]){
                 int j = i;
-                // pq.push(nt[i-1]);
+
                 ans += nt[i-1];
                 int mx = nt[i-1];
                 while(colors[j] == colors[j-1] && j < n){
-                    // pq.push(nt[j]);
+
                     mx = max(mx, nt[j]);
                     ans += nt[j];
                     j++;
                 }
-                // pq.pop();
+
                 ans -= mx;
-                // while(!pq.empty()){
-                //     ans += pq.top();
-                //     pq.pop();
-                // }
+
                 i = j;
             }
         }
