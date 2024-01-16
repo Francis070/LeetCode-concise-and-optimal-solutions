26eb8d9022de2b35851526aa8ed3314db49af8b8
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-01-17 00:14:57 +0530

    Time: 1148 ms (5.01%) | Memory: 293.3 MB (5.05%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
+
+    int rec(int ind, int prev, vector<int>& nums, vector<vector<int>> &dp){
+        if(ind >= nums.size())
+            return 0;
+
+        if(prev != -1 && dp[ind][prev] != -1)
+            return dp[ind][prev];
+        int ans = 0;
+        if(prev == -1 || nums[ind] > nums[prev]){
+            ans = max(ans, 1 + rec(ind + 1, ind, nums, dp));
+        }
+
+        ans = max(ans, rec(ind + 1, prev, nums, dp));
+
+        if(prev == -1)
+            return ans;
+        else{
+            return dp[ind][prev] = ans;
+        }
+
+        // return dp[ind][prev] = ans;
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+        int len = rec(0, -1, nums, dp);
 
-        vector<int> v;
-
-        for(int i =0; i < n; i++){
-            int ele = nums[i];
-
-            int pos = lower_bound(v.begin(), v.end(), ele) - v.begin();
-            if(pos == v.size()) v.push_back(ele);
-            else{
-                v[pos] = ele;
-            }
-
-           
-        }
-         return v.size();
+        return len;
     }
 };
