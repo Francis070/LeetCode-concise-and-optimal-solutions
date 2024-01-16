4eec77507f74a26b55c5517db1890eb56578f5b9
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-01-16 14:08:20 +0530

    Time: 66 ms (49.91%) | Memory: 28.8 MB (55.61%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-
-    int lcs(string s, string t){
+    int minInsertions(string s) {
         int n = s.size();
-        vector<int> prev(n + 1, 0), cur(n + 1, 0);
 
+        string r = s;
+        reverse(r.begin(), r.end());
+        vector<vector<int>> v(n + 1, vector<int>(n + 1, 0));
         for(int i = 1; i <= n; i++){
             for(int j = 1; j<= n; j++){
-                if(s[i-1] == t[j-1]){
-                    cur[j] = 1 + prev[j-1];
+                if(s[i-1] == r[j-1]){
+                    v[i][j] = max(v[i-1][j-1] + 1, max(v[i-1][j], v[i][j-1]) );
                 }
                 else{
-                    cur[j] = max(prev[j], cur[j-1]);
+                    v[i][j] = max(v[i-1][j], v[i][j-1]);
                 }
             }
-            prev = cur;
         }
 
-        return cur[n];
-
-    }
-
-    int minInsertions(string s) {
-        string t = s;
-        reverse(begin(s), end(s));
-
-        int len = lcs(s, t);
-
-        return s.size() - len;
+        int val = v[n][n];
+        return n - val;
     }
 };
