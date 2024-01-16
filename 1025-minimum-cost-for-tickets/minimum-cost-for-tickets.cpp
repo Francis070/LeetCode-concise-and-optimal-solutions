7ef9d4749253087507d1237cba4c51ab37838e57
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-01-16 17:47:46 +0530

    Time: 8 ms (12.27%) | Memory: 20 MB (5.49%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int dp[400][400];
-    int rec(int ind, int tillDay, int n, vector<int>& days, vector<int>& costs){
-        if(ind >= n)
-            return 0;
-        if(dp[ind][tillDay] != -1)
-            return dp[ind][tillDay];
-        int ans = INT_MAX;
-        if(tillDay < days[ind]){
-            for(int i = 0; i < 3; i++){
-                int further = days[ind];
-                if(i == 1)
-                    further += 6;
-                else if(i == 2)
-                    further += 29;
-                
-                ans = min(ans, costs[i] + rec(ind + 1, further, n, days, costs));
-            }
-        }
-        else{
-            ans = min(ans, rec(ind + 1, tillDay, n, days, costs));
-        }
 
-        return dp[ind][tillDay] = ans;
+    int rec(int ind, int md, vector<int> &days, vector<int>& costs, vector<vector<int>> &dp){
+        if(ind >= days.size())
+            return 0;
+
+        if(dp[ind][md] != -1)
+            return dp[ind][md];
+
+        int ans = INT_MAX;
+        if(md >= days[ind]){
+            ans = min(ans, rec(ind + 1, md, days, costs, dp));
+        }
+        int odp = costs[0] + rec(ind + 1, days[ind], days, costs, dp);
+        int sdp = costs[1] + rec(ind + 1, days[ind] + 6, days, costs, dp);
+        int tdp = costs[2] + rec(ind + 1, days[ind] + 29, days, costs, dp);
+
+        ans = min(ans, min(odp, min(sdp, tdp)));
+
+        return dp[ind][md] = ans;
     }
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        memset(dp, -1, sizeof(dp));
-        int ans = rec(0, 0, n, days, costs);
 
-        return ans;
+        vector<vector<int >> dp(n + 2, vector<int>(400, -1));
+        return rec(0, 0, days, costs, dp);
     }
 };
