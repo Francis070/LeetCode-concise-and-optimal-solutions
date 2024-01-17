ec991fdf16a217db8f21b8d4f61f4877efaff5b8
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-01-17 21:29:47 +0530

    Time: 1145 ms (5.01%) | Memory: 293.2 MB (5.05%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
 
-    int rec(int ind, int prev, vector<int>& nums, vector<vector<int>> &dp){
-        if(ind >= nums.size())
+    int rec(int ind, int prev, int n, vector<int> & nums, vector<vector<int>> &dp){
+        if(ind >= n)
             return 0;
-
         if(prev != -1 && dp[ind][prev] != -1)
             return dp[ind][prev];
         int ans = 0;
-        if(prev == -1 || nums[ind] > nums[prev]){
-            ans = max(ans, 1 + rec(ind + 1, ind, nums, dp));
+        if(prev == -1){
+            ans = max(ans, 1 + rec(ind + 1, ind, n, nums, dp));
         }
-
-        ans = max(ans, rec(ind + 1, prev, nums, dp));
-
-        if(prev == -1)
-            return ans;
-        else{
+        else if(nums[ind] > nums[prev]){
+            ans = max(ans,  1 + rec(ind + 1, ind, n, nums,dp));
+        }
+        ans = max(ans,  rec(ind + 1, prev, n, nums, dp));
+        if(prev != -1)
             return dp[ind][prev] = ans;
-        }
-
-        // return dp[ind][prev] = ans;
+        return ans;
     }
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
-        int len = rec(0, -1, nums, dp);
-
-        return len;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        int ans = rec(0, -1, n, nums, dp);
+        return ans;
     }
 };
