6bd1d3c5c857d223ab29c6642428818d4f61cf7e
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-01-21 09:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,27 +3,28 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 0);
-        // int val, p = -1, pp = -1;
-        // pp = nums[0];
-        // p = (n > 1) ? max(nums[1], nums[0]) : -1;
+        int val, p = -1, pp = -1;
+        pp = nums[0];
+        p = (n > 1) ? max(nums[1], nums[0]) : pp;
 
-        // val = max(p, pp);
-
-        // for(int i = 2; i < n; i++){
-        //     pp += nums[i];
-        //     val = max(p, pp);
-        //     swap(pp, p);
-        // }
-
-        // return val;
-
-        dp[0] = nums[0];
-        if(n > 1)
-            dp[1] = max(nums[1], nums[0]);
+        val = max(p, pp);
 
         for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], nums[i] + dp[i - 2]);
+            pp += nums[i];
+            val = max(p, pp);
+            pp = p;
+            p = val;
         }
-        return dp[n-1];
+
+        return val;
+
+        // dp[0] = nums[0];
+        // if(n > 1)
+        //     dp[1] = max(nums[1], nums[0]);
+
+        // for(int i = 2; i < n; i++){
+        //     dp[i] = max(dp[i-1], nums[i] + dp[i - 2]);
+        // }
+        // return dp[n-1];
     }
 };
