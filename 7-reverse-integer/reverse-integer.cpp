8e50d1b5039f8c91a003cc0df60b210368e41974
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-01-27 14:33:02 +0530

    Time: 2 ms (39.29%) | Memory: 7.5 MB (5.93%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,23 +1,17 @@
-class Solution { 
-public: 
-    int reverse(int x) { 
-        int t =abs(x); 
-        long long r=0; 
-        // long long r
-        while(t){ 
-            if(r<INT_MAX and r>INT_MIN){ 
-               int re =t%10;
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        int temp = x;
 
-               
-               r= r*10+re; 
-               if(r > (pow(2, 31) - 1) || r < (-1 * (pow(2, 31) - 1))) 
-                    return 0;
-                 t=t/10; 
-        } 
-        } 
-        if(x<0){ 
-           r= -1*r; 
-        } 
-        return r; 
-    } 
+        while(temp){
+            if( (res * 10) + (temp%10) > INT_MAX ||  (res * 10) + (temp%10) < INT_MIN)
+                return 0;
+
+            res = (res * 10) + (temp%10);
+            temp /= 10;
+        }
+
+        return res;
+    }
 };
