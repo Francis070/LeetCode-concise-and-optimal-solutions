5b53ca12a59cd53982959407eb97dfacbec5605d
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-01-28 11:30:03 +0530

    Time: 162 ms (99.26%) | Memory: 11.9 MB (85.01%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -1,43 +1,51 @@
 class Solution {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& mat, int target) {
-        int r = mat.size(), c = mat[0].size();
+    int numSubmatrixSumTarget(vector<vector<int>>& mat, int tar) {
+        int n = mat.size(), m = mat[0].size();
+
+        vector<vector<int>> vc(n, vector<int>(m, 0));
         int ans = 0;
-        vector<vector<int>> vc(r, vector<int>(c, 0));
 
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
-                vc[i][j] += mat[i][j];
-                if(i-1 >= 0)
-                    vc[i][j] += vc[i - 1][j];
-                if(j - 1 >= 0)
-                    vc[i][j] += vc[i][j-1];
-                if(i - 1 >= 0 && j - 1 >= 0)
-                    vc[i][j] -= vc[i-1][j-1];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                vc[i][j] = mat[i][j];
 
-                if(vc[i][j] == target)
+                if(i - 1 >=0) vc[i][j] += vc[i-1][j];
+                if(j - 1 >= 0) vc[i][j] += vc[i][j-1];
+                if(i - 1 >= 0 && j - 1 >= 0) vc[i][j] -= vc[i-1][j-1];
+
+                if(vc[i][j] == tar )
                     ans++;
             }
         }
 
-        for(int i =0 ;i < r; i++){
-            for(int j = 0; j < c; j++){
-                
-                for(int q = 0; q < j; q++){
-                    if(vc[i][j] - vc[i][q] == target)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++)
+                cout<<vc[i][j]<<" ";
+            cout<<endl;
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j= 0; j < m; j++){
+
+                int val = vc[i][j];
+
+                 for(int q = 0; q < j; q++){
+                    if(vc[i][j] - vc[i][q] == tar)
                         ans++;
                 }
                 for(int q = 0; q < i; q++){
-                    if(vc[i][j] - vc[q][j] == target)
+                    if(vc[i][j] - vc[q][j] == tar)
                         ans++;
                 }
 
                 for(int a = 0; a < i; a++){
                     for(int b = 0; b < j; b++){
-                        if((vc[i][j] - vc[a][j] - vc[i][b] + vc[a][b]) == target)
+                        if((vc[i][j] - vc[a][j] - vc[i][b] + vc[a][b]) == tar)
                             ans++;
                     }
                 }
+
             }
         }
 
