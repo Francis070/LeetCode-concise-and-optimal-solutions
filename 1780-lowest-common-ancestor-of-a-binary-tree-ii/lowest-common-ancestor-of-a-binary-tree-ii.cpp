a5c2298b10d0b15a469ea1ad5ed5b8d9e9a3cb36
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-02-07 23:14:34 +0530

    Time: 95 ms (94.59%) | Memory: 60.3 MB (89.19%) - LeetSync

---
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool flca(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode * &ans){
+        if(root == NULL)
+            return false;
+
+        bool lc = flca(root->left, p, q, ans);
+        bool rc = flca(root->right, p, q, ans);
+
+        if( (lc && rc) || ((root->val == p->val || root->val == q->val) && (lc || rc) ) ){
+            ans = root;
+        }
+
+        if(root->val == p->val || root->val == q->val)
+            return true;
+        return lc || rc;
+    }
+
+    bool find(TreeNode * root, TreeNode * a){
+        if(root == NULL)
+            return false;
+
+        if(root->val == a->val)
+            return true;
+
+        return find(root->left, a) || find(root->right, a);
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // bool a = find(root, p)
+        if(find(root, p) && find(root, q)){
+            TreeNode * ans = NULL;
+            bool x = flca(root, p, q, ans);
+            return ans;
+        }
+        return NULL;
+    }
+};
