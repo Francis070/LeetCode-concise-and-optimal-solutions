dd009423f07fe64c44e837bb82704d7cc6afbd19
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-02-08 08:55:09 +0530

    Time: 95 ms (64.76%) | Memory: 10.7 MB (29.04%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> st;
-        int k = 1;
-        while(k*k <= n){
-            st.push_back(k*k);
-            k++;
+
+        vector<int> ps;
+        for(int i = 1; i*i <= n; i++){
+            ps.push_back(i*i);
         }
 
-        vector<int> dp(n + 1, 1e8);
-        dp[0] = 1;
-        for(auto d : st){
-            if(d <= n)
-                dp[d] = 1;
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for(int i = 1; i*i <= n; i++){
+            dp[i*i]= 1;
         }
+
         for(int i = 1; i <= n; i++){
-            for(int j = 0; j < st.size(); j++){
-                if(st[j] <= i && dp[i - st[j]] != 1e8){
-                    dp[i] = min(dp[i], 1 + dp[i - st[j]]);
+            if(dp[i] == INT_MAX){
+                for(int j = 0; j < ps.size(); j++){
+                    if(ps[j] <= i && dp[i - ps[j]] != INT_MAX){
+                        dp[i] = min(dp[i], 1 + dp[i - ps[j]]);
+                    }
                 }
-
             }
         }
 
