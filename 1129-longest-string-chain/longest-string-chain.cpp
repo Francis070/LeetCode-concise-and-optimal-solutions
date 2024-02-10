4ec20c0202f77813ca8b4e070ab52e083c94d657
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-02-10 19:48:06 +0530

    Time: 57 ms (81.45%) | Memory: 16.8 MB (71.14%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,53 +1,46 @@
 class Solution {
 public:
 
-    static bool comp(string a, string b){
-        if(a.size() != b.size())
-            return a.size() < b.size();
-        
-        return a < b;
+    static bool comp(string &a, string &b){
+        if(a.size() == b.size())
+            return a < b;
+        return a.size() < b.size();
     }
 
-    bool isSub(string s, string t){   // function to find whether s is a sub-sequence of t and s.size() + 1 == t.size()
-        int n1 = s.size(), n2 = t.size();
-
-        if(n2 != n1 + 1)
+    bool is_pred(string & a, string &b){
+        if(a.size() + 1 != b.size())
             return false;
 
         int i = 0, j = 0;
+        int as = a.size(), bs = b.size();
 
-        while(i < n2){
-            if(t[i] == s[j]){
-                j++;
+        while(j < bs){
+            if(a[i] == b[j]){
+                i++;
             }
-            i++;
+            j++;
         }
-
-        if(j >= n1)
+        if(i == as)
             return true;
         return false;
     }
 
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        sort(words.begin(), words.end(), comp); // sorting the words as we need to traverse from backwards and check the longest chain length.
 
-        vector<int> dp(n, 0);  // in dp vector we are going to store the value of max chain length from pos i.
-        dp[n-1] = 1;
-
-        for(int i = n-2; i >= 0; i--){
-            int mx = 1;
-            for(int j = i + 1; j < n; j++){
-                if(isSub(words[i], words[j]))
-                    mx = max(mx, 1 + dp[j]);
+        vector<int> dp(n, 1);
+        sort(words.begin(), words.end(), comp);
+        
+        for(int i = 1; i < n; i++){
+            
+            for(int j = i - 1; j >= 0; j--){
+                if(is_pred(words[j], words[i])){
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
             }
-            dp[i] = mx;
         }
 
-        int ans = 1;
-        for(int i = 0; i < n; i++)  // finding the max value from all the indices and storing in ans.
-            ans = max(ans, dp[i]);
-
+        int ans = *max_element(dp.begin(), dp.end());
         return ans;
     }
 };
