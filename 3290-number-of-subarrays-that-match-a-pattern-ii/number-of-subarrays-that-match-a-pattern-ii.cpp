d849de0f2d11f402cba9e8bd84971cd611010f7b
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-02-11 10:56:26 +0530

    Time: 181 ms (20.00%) | Memory: 216.5 MB (20.00%) - LeetSync

---
+++ 3290-number-of-subarrays-that-match-a-pattern-ii/number-of-subarrays-that-match-a-pattern-ii.cpp
@@ -0,0 +1,92 @@
+class Solution {
+public:
+
+    void KMPSearch(vector<int> & pat, vector<int> & txt, int & ans)
+    {
+        int M = pat.size();
+        int N = txt.size();
+    
+        // create lps[] that will hold the longest prefix suffix
+        // values for pattern
+        int lps[M];
+    
+        // Preprocess the pattern (calculate lps[] array)
+        computeLPSArray(pat, M, lps);
+    
+        int i = 0; // index for txt[]
+        int j = 0; // index for pat[]
+        while ((N - i) >= (M - j)) {
+            if (pat[j] == txt[i]) {
+                j++;
+                i++;
+            }
+    
+            if (j == M) {
+                // printf("Found pattern at index %d ", i - j);
+                ans++;
+                j = lps[j - 1];
+            }
+    
+            // mismatch after j matches
+            else if (i < N && pat[j] != txt[i]) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+    }
+    
+    // Fills lps[] for given pattern pat[0..M-1]
+    void computeLPSArray(vector<int> & pat, int M, int* lps)
+    {
+        // length of the previous longest prefix suffix
+        int len = 0;
+    
+        lps[0] = 0; // lps[0] is always 0
+    
+        // the loop calculates lps[i] for i = 1 to M-1
+        int i = 1;
+        while (i < M) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
+    
+                    // Also, note that we do not increment
+                    // i here
+                }
+                else // if (len == 0)
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
+        int n = nums.size();
+
+        vector<int > org;
+        for(int i = 1; i < n; i++){
+            int x = (nums[i] != nums[i-1]) ? ( (nums[i] < nums[i-1]) ? -1 : 1 ) : 0;
+            org.push_back(x);
+        }
+
+        int ans = 0;
+        KMPSearch(pattern, org, ans);
+
+        return ans;
+    }
+};
