9e56343aaa7bac41b32796561ccf196cca5c86a3
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-02-22 09:55:55 +0530

    Time: 121 ms (84.95%) | Memory: 64.3 MB (69.38%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> whoTrusts(n + 1, 0);
-        vector<int> whoIsTrusted(n + 1, 0);
+        vector<pair<int, int>> trust_and_beTrusted(n + 1, {0, 0});
 
-        for(int i = 0; i < trust.size(); i++){
-            whoTrusts[trust[i][0]]++;
-            whoIsTrusted[trust[i][1]]++;
+        for(int i =0; i < trust.size(); i++){
+            int truster = trust[i][0];
+            int trustee = trust[i][1];
+            trust_and_beTrusted[truster].first++;
+            trust_and_beTrusted[trustee].second++;
         }
 
-        int ans = -1;
-        for(int i = 1; i <= n; i++)
-            if(whoTrusts[i] == 0 && whoIsTrusted[i] == n-1)
-                ans = i;
-        
-        return ans;
+        for(int i =1; i <= n; i++){
+            if(trust_and_beTrusted[i] == make_pair(0, n-1)){
+                return i;
+            }
+        }
+
+        return -1;
     }
 };
