cfaef8ae6673eae57d7c6a3bd52bdfa84ce6868e
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-03-02 11:59:22 +0530

    Time: 33 ms (18.57%) | Memory: 29.4 MB (38.03%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -3,16 +3,35 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
 
-        priority_queue<int, vector<int>, greater<int>> min_heap;
-        
-        for(int i : nums){
-            min_heap.push(i * i);
+        int pos = -1, val = INT_MAX;
+        for(int i = 0; i < n; i++){
+            if(abs(nums[i]) < val){
+                pos = i;
+                val = abs(nums[i]);
+            }
+        }
+        vector<int> res;
+        int left = pos, right = pos + 1;
+        while(left >= 0 && right < n){
+            if(pow(nums[left], 2) <= pow(nums[right], 2)){
+                res.push_back(pow(nums[left], 2));
+                left--;
+            }
+            else{
+                res.push_back(pow(nums[right], 2));
+                right++;
+            }
         }
 
-        vector<int> res;
-        while(!min_heap.empty()){
-            res.push_back(min_heap.top());
-            min_heap.pop();
+        if(left >= 0){
+            for(int i = left; i>= 0; i--){
+                res.push_back(pow(nums[i], 2));
+            }
+        }
+        if(right < n){
+            for(int i = right; i < n; i++){
+                res.push_back(pow(nums[i], 2));
+            }
         }
 
         return res;
