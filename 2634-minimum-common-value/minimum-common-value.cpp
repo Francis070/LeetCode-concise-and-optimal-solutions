5dc414accf6d5f1d5840876f067ce564ebaccc83
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-03-09 12:46:36 +0530

    Time: 73 ms (49.53%) | Memory: 52.9 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -4,11 +4,19 @@ public:
         // sort(begin(nums1), end(nums1));
         // sort(begin(nums2), end(nums2));
 
-        for(int i : nums1){
-            if(binary_search(begin(nums2), end(nums2), i))
-                return i;
+        if(nums1.size() < nums2.size()){
+            for(int i : nums1){
+                if(binary_search(begin(nums2), end(nums2), i))
+                    return i;
+            }
         }
-
+        else{
+            for(int i : nums2){
+                if(binary_search(begin(nums1), end(nums1), i))
+                    return i;
+            }
+        }
+        
         return -1;
     }
 };
