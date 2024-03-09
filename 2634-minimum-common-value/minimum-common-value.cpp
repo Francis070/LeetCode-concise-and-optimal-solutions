72beaae15a2139852d4e728df0b521d70b1a1b26
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-03-09 12:45:29 +0530

    Time: 82 ms (29.65%) | Memory: 52.9 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        sort(begin(nums1), end(nums1));
-        sort(begin(nums2), end(nums2));
+        // sort(begin(nums1), end(nums1));
+        // sort(begin(nums2), end(nums2));
 
         for(int i : nums1){
             if(binary_search(begin(nums2), end(nums2), i))
