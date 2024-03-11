fb25eb58da244e033f673476627ecfd37ec7b765
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-03-11 08:30:59 +0530

    Time: 4 ms (26.39%) | Memory: 8.9 MB (12.46%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -3,23 +3,22 @@ public:
     string customSortString(string order, string s) {
         unordered_map<char, int> um;
 
-        for(char x : s)
-            um[x]++;
+        for(int i = 0; i < order.size(); i++)
+            um[order[i]] = i;
+        
+        vector<pair<int, char>> vc;
         string res = "";
-        for(char x : order ){
-            if(um[x] > 0){
-                for(int i = 0; i < um[x]; i++)
-                    res += x;
 
-                um[x] = 0;
-            }
+        for(char c : s){
+            if(um.find(c) != um.end())
+                vc.push_back({um[c], c});
+            else
+                res += c;
         }
 
-        for(auto x : um){
-            if(x.second > 0){
-                for(int i = 0; i < x.second; i++)
-                    res += x.first;
-            }
+        sort(begin(vc), end(vc), greater<pair<int, char>>());
+        for(int i = 0; i < vc.size(); i++){
+            res = vc[i].second + res;
         }
 
         return res;
