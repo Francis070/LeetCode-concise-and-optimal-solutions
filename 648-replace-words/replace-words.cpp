0207600612e732788064da4d6989721b4ff24dab
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-06-07 12:46:17 +0530

    Time: 1077 ms (7.09%) | Memory: 479.7 MB (11.62%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,103 +1,47 @@
-struct Node {
-    Node * next_node[26];
-    bool is_end;
-
-    Node(){
-        for(int i = 0; i < 26; i++)
-            next_node[i] = NULL;
-        is_end = false;
-    }
-};
-
-class Trie{
-public:
-
-    Node * source = new Node();
-
-    Trie(){
-
-    }
-
-    void insert(string root){
-        Node * temp = source;
-
-        for(char c : root){
-            
-            if(temp->next_node[c - 'a'] == NULL){
-                Node * new_node = new Node();
-                temp->next_node[c - 'a'] = new_node;
-                temp = new_node;
-            }
-            else{
-                temp = temp->next_node[c - 'a'];
-            }
-        }
-        // cout<<endl;
-
-        temp->is_end = true;
-    }
-
-    string search(string word){
-        string res = "";
-        Node * temp = source;
-        for(int i = 0; i < word.size(); i++){
-            if(temp->next_node[word[i] - 'a'] == NULL){
-                if(temp->is_end == true)
-                    return res;
-                else
-                    return "";
-            }
-            else{
-                
-                if(temp->is_end){
-                    return res;
-                }
-                else{
-                    res += word[i];
-                    temp = temp->next_node[word[i] -'a'];
-                }
-            }
-        }
-        return res;
-    }
-
-};
-
 class Solution {
 public:
-    string replaceWords(vector<string>& dictionary, string sen) {
-        Trie *all_words = new Trie();
-
-        for(string word : dictionary){
-            all_words->insert(word);
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        map<string, int> mp;
+        for(string a : dictionary){
+            mp[a]++;
         }
-        string res = "";
-        string cur = "";
-        for(int i = 0; i < sen.size(); i++){
-            if(sen[i] == ' ') {
-                string get_w = all_words->search(cur);
-                // cout<<get_w<<endl;
-                if(get_w == ""){
-                    res += cur;
-                }
-                else{
-                    res += get_w;
-                }
-                res += ' '; 
-                cur = "";
+        string cur;
+        string res;
+        int n = sentence.size();
+        for(int i =0;i < n; i++){
+            if(sentence[i] != ' '){
+                cur += sentence[i];
             }
             else{
-                cur += sen[i];
+                bool chk = false;
+                for(int j = 0; j < cur.size(); j++){
+                    if(mp.count(cur.substr(0, j + 1)) == 1){
+                        chk = true;
+                        res += cur.substr(0, j + 1);
+                        break;
+                    }
+                }
+                if(chk == false){
+                    res += cur;
+                }
+                res += " ";
+                cur = "";
             }
         }
 
-        string get_w = all_words->search(cur);
-        if(get_w == ""){
+        bool er = false;
+        for(int j = 0; j < cur.size(); j++){
+            if(mp.count(cur.substr(0, j + 1)) == 1){
+                er = true;
+                res += cur.substr(0, j + 1);
+                break;
+            }
+        }
+        if(er == false){
             res += cur;
         }
-        else{
-            res += get_w;
-        }
+
+        // res = res.substr(0, res.size() - 1);
 
         return res;
     }
