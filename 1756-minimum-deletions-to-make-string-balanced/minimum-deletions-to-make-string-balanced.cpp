9dac53f7a13847b0b88ad5c28777c28707db47fd
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-07-30 15:00:44 +0530

    Time: 87 ms (65.07%) | Memory: 38.5 MB (37.32%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -3,31 +3,26 @@ public:
     int minimumDeletions(string s) {
         int n = s.size();
 
-        int ans = 0;
-
-        string t = s;
+        int ca = 0, cb = 0;
+        int ans = INT_MAX;
+        vector<int> dp(n, 0);
         for(int i = 0; i < n; i++){
-            if(s[i] == 'b'){
-                t = t.substr(i, n - i);
-                break;
-            }
-        }
-        int cnt_a = 0, cnt_b = 0;
-        for(int i = 0; i < t.size(); i++){
-            if(t[i] == 'a'){
-                cnt_a++;
-                
-                if(cnt_b > 0){
-                    ans = min(min(cnt_a, cnt_b), ans + 1);
+            if(s[i] == 'a') ca++;
+            if(s[i] == 'b') cb++;
+
+            if(i > 0){
+                if(s[i] == 'a' ){
+                    if(cb > 0)
+                        dp[i] = min(cb, dp[i-1] + 1);
+                    else 
+                        dp[i] = dp[i-1];
+                }
+                else{
+                    dp[i] = dp[i-1];
                 }
             }
-            else{
-                cnt_b++;
-            }
         }
 
-        // ans = min(cnt_a, cnt_b);
-
-        return ans;
+        return dp[n-1];
     }
 };
