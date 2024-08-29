5129e33e0aa3fdf8db711a772d09d37d5b927daf
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-08-30 00:40:54 +0530

    Time: 78 ms (27.09%) | Memory: 31.3 MB (5.07%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    map<int, vector<pair<int, int>>> rows, cols;
+    map<int, map<int, int>> mp;
+
+    void dfs(int i, int j) {
+        
+        if(mp[i][j] == 0) return;
+        mp[i][j] = 0;
+        
+        for(auto n : rows[i]) dfs(n.first, n.second);
+        for(auto n : cols[j]) dfs(n.first, n.second);
+    }
+
+    int removeStones(vector<vector<int>>& g) {
+
+        for(auto n : g) {
+            
+            int i = n[0]; int j = n[1];
+            rows[i].push_back({i, j});
+            cols[j].push_back({i, j});
+            mp[i][j] = 1;
+        }
+        
+        int n = 0;
+        
+        for(auto e : g) 
+            if(mp[e[0]][e[1]]) dfs(e[0], e[1]), n++;
+        
+        return g.size() - n;
+    }
+};
