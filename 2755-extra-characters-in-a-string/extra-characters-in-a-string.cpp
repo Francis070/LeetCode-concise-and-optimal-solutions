60709c5bdab47a2f7a27e5194931490b00a8270c
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-09-23 12:57:29 +0530

    Time: 67 ms (92.06%) | Memory: 69.2 MB (74.66%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,58 +1,40 @@
 class Solution {
 public:
 
-    int rec(int ind, string s, unordered_map<string, int> &um, int n, vector<int> &dp){
-        if(ind == n)
+    int rec(int ind, string &s,  map<char, vector<string>> &mp, vector<int> & dp){
+        int n = s.size();
+        if(ind >= n){
             return 0;
+        }
 
         if(dp[ind] != -1)
             return dp[ind];
 
-        string temp = "";
-        int ans = s.size();
-        for(int i= ind; i < n; i++){
-            temp += s[i];
-            int chk = rec(i + 1, s, um, n, dp);
-
-            int cur = temp.size();
-            if(um[temp] > 0)
-                cur = 0;
-
-            ans = min(ans, cur + chk);
-        }
-
-        return dp[ind] = ans;
-    }
-    
-    int minExtraChar(string s, vector<string>& dic) {
-        int n = s.size();
-        unordered_map<string, int> um;
-        vector<int> dp(n + 1, 0);
-
-        for(string x : dic){
-            um[x]++;
-        }
-
-        // int ans = rec(0, s, um, n, dp);
-
-        for(int i = n-1; i >= 0; i--){
-            string temp = "";
-            int ans = s.size();
-            for(int j= i; j < n; j++){
-                temp += s[j];
-                // int chk = rec(j + 1, s, um, n, dp);
-                int chk = dp[j + 1];
-
-                int cur = temp.size();
-                if(um[temp] > 0)
-                    cur = 0;
-
-                ans = min(ans, cur + chk);
+        int cur = n - ind;
+        if(mp.find(s[ind]) != mp.end()){
+            for(auto & x : mp[s[ind]]){
+                if((n - ind) >= x.size() && x == s.substr(ind, x.size())){
+                    cur = min(cur, rec(ind + x.size(), s, mp, dp));
+                }
             }
+        }
+        
+        cur = min(cur, 1 + rec(ind + 1, s, mp, dp));
+        
+        return dp[ind] = cur;
+    }
 
-            dp[i] = ans;
+    int minExtraChar(string s, vector<string>& dictionary) {
+        map<char, vector<string>> mp;
+        int n = s.size();
+        for(auto x : dictionary){
+            mp[x[0]].push_back(x);
         }
 
-        return dp[0];
+        vector<int> dp(n, -1);
+
+        int ans = rec(0, s, mp, dp);
+
+        return ans;
     }
 };
