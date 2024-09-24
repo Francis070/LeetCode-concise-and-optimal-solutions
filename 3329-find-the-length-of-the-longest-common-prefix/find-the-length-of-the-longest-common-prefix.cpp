67ca2c184c05faf9e3a74b5d34866a79629fce3c
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-09-24 13:44:31 +0530

    Time: 226 ms (87.18%) | Memory: 169.8 MB (42.66%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,31 +1,86 @@
+struct Node {
+    Node* links[10];
+
+    bool flag = false;
+
+    bool containsKey(char ch) {
+        return links[ch - '0'] != NULL;
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - '0'] = node;
+    }
+
+    Node* get(char ch) {
+        return links[ch - '0'];
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+
+    int startsWith(string prefix) {
+        Node* node = root;
+        int cnt = 0;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (!node->containsKey(prefix[i])) {
+                return cnt;
+            }
+            cnt++;
+            node = node->get(prefix[i]);
+        }
+        
+        return cnt;
+    }
+};
+
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<string> f, s;
-        
-        for(int w : arr1){
-            string t = to_string(w);
-            string d = "";
-            for(char c : t){
-                d += c;
-                f.insert(d);
-            }
-        }
+        Trie trie;
 
-        for(int w : arr2){
-            string t = to_string(w);
-            string d = "";
-            for(char c : t){
-                d += c;
-                s.insert(d);
-            }
-        }
-        int ans =0 ;
-        for(auto x : s){
-            if(f.find(x) != f.end())
-                ans = max(ans, int(x.size()));
-        }
+        for(int s : arr1)
+            trie.insert(to_string(s));
 
+        int ans = 0;
+        for(int s : arr2)
+            ans = max(ans, trie.startsWith(to_string(s)));
 
         return ans;
     }
