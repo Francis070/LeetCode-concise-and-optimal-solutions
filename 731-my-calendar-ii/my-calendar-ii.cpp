12ab2eda258bfef2ed83fb6c3ddb62a5a4a71e0c
Abhijit Mukherjee <56750393+Francis070@users.noreply.github.com>
2024-09-27 13:16:55 +0530

    Time: 128 ms (79.35%) | Memory: 46.4 MB (5.63%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -0,0 +1,91 @@
+class MyCalendarTwo {
+public:
+    map<int, vector<int>> mp;
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        if(mp.size()== 0){
+            mp[start].push_back(end);
+            return true;
+        }
+
+        auto pose = mp.upper_bound(end);
+        vector<pair<int, int>> pairs;
+
+        
+
+        
+        map<int, vector<int>>::iterator it;
+        for(it = mp.begin(); it != pose; it++){
+            int fr = it->first;
+            for(auto x : it->second){
+                if(!(start >= x || end <= fr)){
+                    pairs.push_back({fr, x});
+                }
+            }
+        }
+
+        // if(start == 11  && end == 19){
+        //     for(auto x : pairs){
+        //         cout<<x.first<<" "<<x.second<<endl;
+        //     }
+        // }
+
+        map<int, int> mpDouble;
+
+        for(auto x : pairs){
+            if(!bookDouble(x.first, x.second, mpDouble)){
+                return false;
+            }
+        }
+
+        mp[start].push_back(end);
+        return true;
+    }
+
+    bool bookDouble(int start, int end, map<int, int> &mpd) {
+        auto x = mpd.lower_bound(start);
+
+        if(x == mpd.begin() && mpd.size() > 0){
+            if(end > x->first){
+                return false;
+            }
+            else {
+                mpd[start] = end;
+                return true;
+            }
+        }
+        else if(x == mpd.end() && mpd.size() > 0){
+            x = prev(x);
+            if(start < x->second)
+                return false;
+            else{
+                mpd[start] = end;
+                return true;
+            }
+        }
+        else{
+            if(mpd.size() == 0){
+                mpd[start] = end;
+                return true;
+            }
+            else {
+                auto y = prev(x);
+                if(start < y->second || end > x->first)
+                    return false;
+                else{
+                    mpd[start] = end;
+                    return true;
+                }
+            }
+        }
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
